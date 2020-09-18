aed445c001539f6a10e9cea0ab6cffc2d0377f81
Golo Roden <golo.roden@thenativeweb.io>
2020-09-18 11:42:42 +0200

    fix: Disable verifying assigning any.

--- node.js
+++ node.js
@@ -960,7 +960,7 @@ const overrides = [
       '@typescript-eslint/no-unnecessary-type-assertion': [ 'error', {
         typesToIgnore: []
       }],
-      '@typescript-eslint/no-unsafe-assignment': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
