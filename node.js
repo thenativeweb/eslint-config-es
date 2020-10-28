1adbc137d474c24d06615fcd345717fa68e0020a
Golo Roden <golo.roden@thenativeweb.io>
2020-10-28 19:27:00 +0100

    fix: Fix ESLint errors.

--- node.js
+++ node.js
@@ -6,6 +6,8 @@ const isInstalled = require('./lib/isInstalled');
 
 const parserOptions = {
   sourceType: 'script',
+
+  // eslint-disable-next-line unicorn/numeric-separators-style
   ecmaVersion: 2021,
   ecmaFeatures: {
     globalReturn: false,
