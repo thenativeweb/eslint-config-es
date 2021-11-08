c4aeb5460084fac54504cbe7739d558383212b30
David Losert <david.losert@gmx.de>
2021-11-08 09:27:04 +0100

    chore: Try to raise timeout for tsx test to fix ci issue

--- test/integration/rules/reactTests.ts
+++ test/integration/rules/reactTests.ts
@@ -1,7 +1,7 @@
 import { assertLint } from '../../shared/assertLint';
 import { lintJavaScript, lintJsx, lintTsx } from '../../shared/esLintTester';
 
-suite('react/', (): void => {
+suite('react/', function (): void {
   test('jsx-fragments: Short <></> form is not allowed.', async (): Promise<void> => {
     const result = await lintJsx(`<><MyComponent /></>`);
 
@@ -9,6 +9,9 @@ suite('react/', (): void => {
   });
 
   test('linting also works in tsx files.', async (): Promise<void> => {
+    // Testing TSX can take a while on CI
+    this.timeout(5_000);
+
     const result = await lintTsx(`<><MyComponent /></>`);
 
     assertLint(result).containsError('react/jsx-fragments');
