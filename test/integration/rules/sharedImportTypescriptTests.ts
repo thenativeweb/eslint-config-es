7015d23e693a42a08ec3eb31f4c82b20557c48ca
David Losert <david.losert@gmx.de>
2021-11-19 16:24:42 +0100

    chore: Fixes wrong test description

--- test/integration/rules/sharedImportTypescriptTests.ts
+++ test/integration/rules/sharedImportTypescriptTests.ts
@@ -8,7 +8,7 @@ suite('sharedImportTypescript', (): void => {
     assertLint(result).notContainsError('import/no-commonjs');
   });
 
-  test('no-common-js: errors for typescript.', async (): Promise<void> => {
+  test('import/no-commonjs: errors for typescript.', async (): Promise<void> => {
     const result = await lintTypescript(`const fs = require('fs');`);
 
     assertLint(result).containsError('import/no-commonjs');
